de015417901e3433a5a84f8001ed4fb18e7651e9
Prakhar Sinha <thesecretinspiration@gmail.com>
2022-08-04 12:08:19 +0530

    added comments

--- register.php
+++ register.php
@@ -20,6 +20,7 @@ $prefix = md5($name.$eater_type.$age);
 $profileid = uniqid($prefix, FALSE);
 /* -------------------------------- */ 
 
+/* connecting to the Database */
 try {
      
     $db = new PDO("mysql:host=localhost;dbname=foodee", "root", "");
@@ -30,7 +31,10 @@ catch(PDOException $e)
 {
     echo "connfailed!";
 }
+/* -------------------------- */
 
+
+/* Inserting the user in to profiles table */ 
 try {
     $query = $db->prepare("INSERT INTO `profiles` VALUES('".$profileid."', '".$name."', '".$eater_type."', '".$age."', '".$gender."', '".$mobile."')");
     $query->execute();
@@ -39,7 +43,7 @@ catch(PDOException $e) {
     echo "query failed ".$e->getMessage();
 }
 
-
+/* Creating an extra table for the user data for future use. */ 
 try {
     $query1 = $db->prepare("
     
