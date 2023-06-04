9a4ed9e8ffa8490221d049ef2e27974b00408ae6
Prakhar Sinha <thesecretinspiration@gmail.com>
2023-06-05 01:00:14 +0530

    Code quality improved of Algo, testing left

--- recommend.php
+++ recommend.php
@@ -5,9 +5,7 @@ define("FOOD_RECOMMENDATION_LIMIT", 6);
 
 class recommend extends db {
 
-    private $tempRecommendationStorage;
-    private $specialDayFrequency;
-    private $normalDayFrequency;
+    private $tempRecommendationStorage, $specialDayFrequency, $normalDayFrequency;
     
     private function __construct() {
         $this->tempRecommendationStorage = array();
@@ -15,35 +13,21 @@ class recommend extends db {
         $this->specialDayFrequency = array_reverse($normalDayFrequency); // special becomes priority
     }
 
-    private function getFoodRecommendation($foodtype_user, $foodtimetype, $specialDay, $declinedfoodarray, $foodfrequency, $limit=FOOD_RECOMMENDATION_LIMIT) {
+    private function getFoodRecommendation($foodtype_user, $foodtimetype, $specialDay, $alreadyShownFoodIDs, $foodfrequency, $limit=FOOD_RECOMMENDATION_LIMIT) {
 
-         /* Idealising declined_food array to use in foodid NOT IN clause */
-         $decfoodarray = json_decode($declinedfoodarray, true);
-         $notinfoodid = '';
-         if(sizeof($decfoodarray)!=0) {
-             $len = sizeof($decfoodarray)-1;
-             foreach($decfoodarray as $key => $value) {
-                 if($len==$key) {
-                     $notinfoodid .= $value;
-                 }
-                 else {
-                     $notinfoodid .= $value.",";
-                 }
-             }
-         }
-         else {
-             $notinfoodid = "''";
-         }
+        /* Idealising declined_food array to use in foodid NOT IN clause */
+        $alreadyShownFoodArray = json_decode($alreadyShownFoodIDs, true);
+        $excludedFoodIDs = implode(',', $alreadyShownFoodArray);
 
         $foodFrequencies = "";
         ($specialDay) ? $foodFrequencies = $this->specialDayFrequency : $foodFrequencies = $this->normalDayFrequency;
 
         try {
             if($foodtype_user=='nonveg') {
-                $query = db::connect()->prepare("SELECT `food_id`, `name`, `image`, `food_type` FROM `commonnorth` WHERE MATCH(`food_time_type`) AGAINST ('".$foodtimetype."' IN NATURAL LANGUAGE MODE) AND `food_frequency_type`='".$foodFrequencies[$foodfrequency]."' AND `food_id` NOT IN (".$notinfoodid.") LIMIT ".$limit." ");
+                $query = db::connect()->prepare("SELECT `food_id`, `name`, `image`, `food_type` FROM `commonnorth` WHERE MATCH(`food_time_type`) AGAINST ('".$foodtimetype."' IN NATURAL LANGUAGE MODE) AND `food_frequency_type`='".$foodFrequencies[$foodfrequency]."' AND `food_id` NOT IN (".$excludedFoodIDs.") LIMIT ".$limit." ");
             }
             else {
-                $query = db::connect()->prepare("SELECT `food_id`, `name`, `image`, `food_type` FROM `commonnorth` WHERE MATCH(`food_time_type`) AGAINST ('".$foodtimetype."' IN NATURAL LANGUAGE MODE) AND `food_frequency_type`='".$foodFrequencies[$foodfrequency]."' AND `food_id` NOT IN (".$notinfoodid.") AND `food_type`='veg' LIMIT ".$limit." ");
+                $query = db::connect()->prepare("SELECT `food_id`, `name`, `image`, `food_type` FROM `commonnorth` WHERE MATCH(`food_time_type`) AGAINST ('".$foodtimetype."' IN NATURAL LANGUAGE MODE) AND `food_frequency_type`='".$foodFrequencies[$foodfrequency]."' AND `food_id` NOT IN (".$excludedFoodIDs.") AND `food_type`='veg' LIMIT ".$limit." ");
             }
         }
         catch(PDOException $e) {
@@ -58,7 +42,7 @@ class recommend extends db {
         
         if($resultCount<6) {
             if($foodFrequency+1 <= 3) { // 3 is the count of normalDayFrequency Array
-                $this->getFoodRecommendation($foodtype_user, $foodtimetype, $specialDay, $declinedfoodarray, $foodFrequency+1, $limit-$resultCount);
+                $this->getFoodRecommendation($foodtype_user, $foodtimetype, $specialDay, $alreadyShownFoodIDs, $foodFrequency+1, $limit-$resultCount);
             }
         }
         
@@ -69,14 +53,14 @@ class recommend extends db {
     public function launchAPI() {
         $foodtype_user = (isset($_POST['foodtype_user'])) ? $_POST['foodtype_user'] : 0;
         $foodtimetype = (isset($_POST['food_time_type'])) ? $_POST['food_time_type'] : 0;
-        $declinedfoodarray = (isset($_POST['declined_food_array'])) ? $_POST['declined_food_array'] : 0;
+        $alreadyShownFoodIDs = (isset($_POST['declined_food_array'])) ? $_POST['declined_food_array'] : 0;
 
-        if(!$foodtimetype || !$foodtimetype || !$declinedfoodarray) {
+        if(!$foodtimetype || !$foodtimetype || !$alreadyShownFoodIDs) {
             return json_encode(array("status"=>"2", "msg"=>"Food Properties data missing."));
         }
 
         $finalRecommendations = "";
-        (isset($_POST["specialday"])) ? $finalRecommendations = $this->getFoodRecommendation($foodtype_user, $foodtimetype, true, $declinedfoodarray, 0) : $finalRecommendations = $this->getFoodRecommendation($foodtype_user, $foodtimetype, false, $declinedfoodarray, 0); 
+        (isset($_POST["specialday"])) ? $finalRecommendations = $this->getFoodRecommendation($foodtype_user, $foodtimetype, true, $alreadyShownFoodIDs, 0) : $finalRecommendations = $this->getFoodRecommendation($foodtype_user, $foodtimetype, false, $alreadyShownFoodIDs, 0); 
         
         if(!$finalRecommendations) {
             return json_encode(array("status"=>"0", "msg"=>"Error running query!"));
@@ -87,7 +71,6 @@ class recommend extends db {
     }
 
 }
-
 
 $obj = new recommend();
 echo $obj->launchAPI();
