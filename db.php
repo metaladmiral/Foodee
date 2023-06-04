531b27e95b7768ceb2720dc7f7d80ccc5c33eb70
Prakhar Sinha <thesecretinspiration@gmail.com>
2023-06-05 02:14:37 +0530

    Made DB file generalised

--- db.php
+++ db.php
@@ -1,11 +1,16 @@
 <?php
 
 class db {
+
+    private $host = "YOUR_HOST_HERE";
+    private $dbUser = "YOUR_USER_HERE";
+    private $dbPass = "YOUR_PASS_HERE";
+
     public function connect() {
         
         try {
      
-            $db = new PDO("mysql:host=localhost;dbname=foodee", "root", "");
+            $db = new PDO("mysql:host=".$this->host.";dbname=foodee", "$this->dbUser", "$this->dbPass");
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
             return $db;                
